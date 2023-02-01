8a7ebe629b943fcce43d617a5cf729160b4d5b9b
Zohan Subhash <zohan.subhash@gmail.com>
2023-02-01 10:52:19 +0000

    Await (bug)

--- dist/index.js
+++ dist/index.js
@@ -16076,7 +16076,7 @@ const main = async () => {
             }
         }
 
-        const existingComments = octokit.rest.pulls.listCommits({
+        const existingComments = await octokit.rest.pulls.listCommits({
             owner: github.context.repo.owner,
             repo: github.context.repo.repo,
             pull_number: github.context.payload.pull_request.number,
