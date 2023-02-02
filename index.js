ba72ecda3a9e6dc04e321df52387aa52dc89c9c7
Zohan Subhash <zohan.subhash@gmail.com>
2023-02-02 13:13:55 +0000

    Debug

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ const main = async () => {
 
                 let shouldResolve = true
 
-                console.log('reactions', (existingComment.in_reply_to_id ? existingComments[existingCommentIds.indexOf(existingComment.in_reply_to_id)].user.login == 'github-actions[bot]' : false), existingComment.reactions, existingComment.reactions['-1'])
+                console.log('reactions', (existingComment.in_reply_to_id ? existingComments[existingCommentIds.indexOf(existingComment.in_reply_to_id)].user.login == 'github-actions[bot]' : false), existingComment.reactions, existingComments[existingCommentIds.indexOf(existingComment.in_reply_to_id)].user.login == 'github-actions[bot]', existingComment.reactions['-1'] > 0)
 
                 if (existingComment.reactions['-1'] > 0 && (existingComment.in_reply_to_id ? existingComments[existingCommentIds.indexOf(existingComment.in_reply_to_id)].user.login == 'github-actions[bot]' : false)) {
                     resolved.push(existingComment.in_reply_to_id)
